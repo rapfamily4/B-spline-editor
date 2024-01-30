16b815efd25a13913dfb90a98d782194161b6fbb
rapfamily4 <luigirapetta@libero.it>
2024-01-30 19:36:46 +0100

    Added dDebug prints.

--- scenes/spline/spline.gd
+++ scenes/spline/spline.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const SPLINE_DEGREE: int = 2
-const STARTING_CONTROL_POINTS_COUNT: int = 10
+const SPLINE_DEGREE: int = 2 # k
+const STARTING_CONTROL_POINTS_COUNT: int = 10 # n + 1
 
 @onready var line_renderer: Line2D = $LineRenderer
 @onready var control_points_tree: Node2D = $ControlPoints
@@ -13,19 +13,19 @@ var knots: Array[float] # The knot vector is normalized in the interval [0, 1].
 func _ready():
 	# Palcement is not parametrized: it needs refactoring.
 	var window_resolution: Vector2 = get_window().size
-	for i in range(STARTING_CONTROL_POINTS_COUNT):
+	for i: int in range(STARTING_CONTROL_POINTS_COUNT):
 		var new_x: float = i * window_resolution.x * 0.1 + window_resolution.x * 0.05
 		var new_y: float = randf_range(0, window_resolution.y * 0.3) + window_resolution.y * 0.35
 		add_control_point(Vector2(new_x, new_y))
 	
-	for i in range(STARTING_CONTROL_POINTS_COUNT + SPLINE_DEGREE + 1):
+	for i: int in range(STARTING_CONTROL_POINTS_COUNT + SPLINE_DEGREE):
 		if i in range(SPLINE_DEGREE):
 			knots.append(0)
-		elif i in range(STARTING_CONTROL_POINTS_COUNT + 1, STARTING_CONTROL_POINTS_COUNT + SPLINE_DEGREE + 1):
+		elif i in range(STARTING_CONTROL_POINTS_COUNT, STARTING_CONTROL_POINTS_COUNT + SPLINE_DEGREE):
 			knots.append(1)
 		else:
-			knots.append(float(i - SPLINE_DEGREE + 1) / float(STARTING_CONTROL_POINTS_COUNT - SPLINE_DEGREE + 3))
-		print("Knot #" + str(i) + ":    " + str(knots[i]))
+			knots.append(float(i - SPLINE_DEGREE + 1) / float(STARTING_CONTROL_POINTS_COUNT - SPLINE_DEGREE + 1))
+		print("Generated knot #" + str(i) + ":    " + str(knots[i]))
 	
 	update_curve()
 
@@ -40,5 +40,42 @@ func add_control_point(point_position: Vector2 = Vector2.ZERO) -> void:
 func remove_control_point(point: Marker2D) -> void:
 	control_points_tree.remove_child(point)
 
-func update_curve() -> void:
-	pass
+func update_curve(resolution: int = 128) -> void:
+	for i: int in range(resolution + 1):
+		line_renderer.add_point(evaluate_curve(float(i) / float(resolution)))
+
+func evaluate_curve(evaluation_point: float) -> Vector2:
+	var spline_point: Vector2 = Vector2.ZERO
+	var blend_value: float
+	for i: int in range(STARTING_CONTROL_POINTS_COUNT):
+		blend_value = _blend(i, SPLINE_DEGREE, evaluation_point)
+		spline_point += blend_value * control_points_tree.get_child(i).position
+	return spline_point
+
+func _aux0(control_point: int, degree: int, evaluation: float) -> float:
+	var to_return: float = (evaluation - knots[control_point]) / (knots[control_point + degree - 1] - knots[control_point])
+	return to_return
+
+func _aux1(control_point: int, degree: int, evaluation: float) -> float:
+	var to_return: float = (knots[control_point + degree] - evaluation) / (knots[control_point + degree] - knots[control_point + 1])
+	return to_return
+
+func _blend(control_point: int, degree: int, evaluation: float) -> float:
+	var result: float
+	if degree == 1:
+		result = 1 if (knots[control_point] <= evaluation) and (evaluation < knots[control_point + 1]) else 0
+		print("--- control_point: " + str(control_point) + ", degree: " + str(degree) + ", evaluation: " + str(evaluation) + " ---")
+		print("returned " + str(result))
+		return result
+	var aux0: float = _aux0(control_point, degree, evaluation)
+	var aux1: float = _aux1(control_point, degree, evaluation)
+	var blend0: float = _blend(control_point, degree - 1, evaluation)
+	var blend1: float = _blend(control_point + 1, degree - 1, evaluation)
+	result = aux0 * blend0 + aux1 * blend1
+	print("--- control_point: " + str(control_point) + ", degree: " + str(degree) + ", evaluation: " + str(evaluation) + " ---")
+	print("aux0: " + str(aux0))
+	print("aux1: " + str(aux1))
+	print("blend0: " + str(blend0))
+	print("blend1: " + str(blend1))
+	print("returned " + str(result))
+	return result
